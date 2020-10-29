568294f286a0d1db6e364820e253153f27b1fb34
stla <laurent_step@yahoo.fr>
2020-10-29 12:07:40 +0100

    renamed 'pivotal' to 'rGPQ'

--- R/pivotal.R
+++ R/pivotal.R
@@ -5,7 +5,7 @@
 #' @param fit an \code{\link{aov1r}} object
 #' @param n number of simulations
 #'
-#' @return The simulations in a list.
+#' @return The simulations in a dataframe.
 #'
 #' @importFrom stats rchisq
 #' @export
@@ -13,8 +13,9 @@
 #' @examples
 #' dat <- simAOV1R(I=20, J=5, mu=10, sigmab=1, sigmaw=1)
 #' fit <- aov1r(y ~ group, data=dat)
-#' pivsims <- pivotal(fit)
-#' pivsims$G_sigma2tot <- pivsims$G_sigma2b + pivsims$G_sigma2w
+#' nsims <- 20000
+#' pivsims <- rGPQ(fit, nsims)
+#' pivsims$GPQ_sigma2tot <- pivsims$GPQ_sigma2b + pivsims$GPQ_sigma2w
 #' # Generalized confidence intervals:
 #' lapply(pivsims, quantile, probs = c(0.025, 0.975))
 #' # compare with the frequentist confidence intervals:
@@ -22,12 +23,12 @@
 #' # Generalized prediction interval:
 #' with(
 #'   pivsims,
-#'   quantile(rnorm(length(G_mu), G_mu, sqrt(G_sigma2tot)),
+#'   quantile(rnorm(nsims, GPQ_mu, sqrt(GPQ_sigma2tot)),
 #'            probs = c(0.025, 0.975))
 #' )
 #' # compare with the frequentist prediction interval:
 #' predict(fit)
-pivotal <- function(fit, n=10000){
+rGPQ <- function(fit, n=10000){
   I <- fit[["Design"]][["I"]]
   J <- fit[["Design"]][["Jh"]]
   N <- fit[["Design"]][["N"]]
@@ -36,10 +37,10 @@ pivotal <- function(fit, n=10000){
   Z <- rnorm(n)
   U2b <- rchisq(n, I-1)
   U2w <- rchisq(n, N-I)
-  list(
-    G_mu = fit[["grandMean"]] - Z/sqrt(U2b)*sqrt(ssb/I/J),
-    G_sigma2b = 1/J*(ssb/U2b - ssw/U2w),
-    G_sigma2w = ssw/U2w
+  data.frame(
+    GPQ_mu = fit[["grandMean"]] - Z/sqrt(U2b)*sqrt(ssb/I/J),
+    GPQ_sigma2b = 1/J*(ssb/U2b - ssw/U2w),
+    GPQ_sigma2w = ssw/U2w
   )
 }
 
